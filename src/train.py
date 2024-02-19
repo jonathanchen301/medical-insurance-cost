2a2aa6cdd604d21ddb4d7add20ed23cb541c767d
Jonathan <jonathanchen301@gmail.com>
2024-02-19 17:27:02 -0500

    Implement train_test_split

--- src/train.py
+++ src/train.py
@@ -1,6 +1,13 @@
 from dataset import *
+from sklearn.model_selection import train_test_split
 
-# Import the data
+# 80-10-10 split
 
 dataset = InsuranceDataset("data/insurance.csv", transform=EncodingToTensor())
-dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+
+train_data, test_data = train_test_split(dataset, test_size=0.2, random_state=42)
+test_data, dev_data = train_test_split(test_data, test_size=0.5, random_state=42)
+
+train_dataloader = DataLoader(train_data, batch_size=1, shuffle=False)
+dev_dataloader = DataLoader(dev_data, batch_size=1, shuffle=False)
+test_dataloader = DataLoader(test_data, batch_size=1, shuffle=False)
